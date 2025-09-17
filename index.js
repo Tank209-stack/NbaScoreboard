4ed15f6b9221cd6ee3542aa4e829491dffa4de52
Tank209-stack <107075139+Tank209-stack@users.noreply.github.com>
2025-09-17 09:07:37 +0530

    initial code for scoreboard

---
+++ index.js
@@ -0,0 +1,53 @@
+let homeScore = 0
+let home = document.querySelector(".home")
+let awayScore = 0
+let away =  document.querySelector(".away")
+
+  
+    function homeBtnOne(){
+        homeScore+=1;
+        home.textContent = homeScore
+    }
+    
+        function homeBtnTwo(){
+        homeScore+=2;
+        home.textContent = homeScore
+    }
+    
+        function homeBtnThree(){
+        homeScore+=3;
+        home.textContent = homeScore
+    }
+
+        function awayBtnOne(){
+            awayScore+=1;
+            away.textContent = awayScore
+        }  
+        
+        function awayBtnTwo(){
+            awayScore+=2;
+            away.textContent = awayScore
+        } 
+        
+        function awayBtnThree(){
+            awayScore+=3;
+            away.textContent = awayScore
+        } 
+    
+        function reset(){
+            awayScore = 0;
+            away.textContent = awayScore
+        }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
